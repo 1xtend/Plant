4cdc4fa6735a55c2e7aa1c2edfc0983a759f9099
1xtend <vlad.mezhevyi@gmail.com>
2023-03-18 12:52:24 +0200

    fixed body

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const burger = document.querySelector('.burger');
 const navbar = document.querySelector('.navbar');
 const navbarList = navbar.querySelector('.navbar__list');
+const body = document.body;
 
 burger.addEventListener('click', (e) => {
   burger.classList.toggle('is-active');
   navbar.classList.toggle('is-active');
+  body.classList.toggle('is-fixed');
 
   navbarList.addEventListener('click', (e) => {
     if (e.target !== e.currentTarget) {
       burger.classList.remove('is-active');
       navbar.classList.remove('is-active');
+      body.classList.remove('is-fixed');
     }
   });
 });
